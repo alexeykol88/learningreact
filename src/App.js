b9ea4c166d9c0ae5b79aedc79bcb3404a3bd27d3
Alexey <aleshakol@ukr.net>
2019-01-12 04:21:43 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Column from './column';
+
+const trying = "Trying something in React";
+
+function App(){
+  return (
+    <div className = "container">
+    <h1 className = "trying">{trying}</h1>
+      <Column className= "col1"/>
+      <Column className= "col2"/>
+      <Column className="col3"/>
+      <Column className="col4"/>
+    </div>
+    
+  )
+}
+
+export default App;
---
+++ src/column.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './column.css'
+
+export default class Column extends Component {
+  render() {
+    return (
+      <div className = "childdiv">
+        <h1>Column</h1>
+      </div>
+    )
+  }
+}
